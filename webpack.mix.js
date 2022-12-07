c719b25ec1b0ac03d72963cab98ae26ca75c4096
Sebastian Klinge <github@derklinge.de>
2022-12-07 19:06:27 +0100

    Add CSS `url()`-directives processing (fonts etc.)
    Signed-off-by: Sebastian Klinge <github@derklinge.de>

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,17 +2,21 @@ const mix = require('laravel-mix');
 require('laravel-mix-jigsaw');
 
 mix.disableSuccessNotifications();
+
 mix.setPublicPath('source/assets/build');
+
+mix.setResourceRoot('/assets/build');
 
 mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
     .css('source/_assets/css/main.css', 'css', [
         require('postcss-import'),
+        require('postcss-url'),
         require('tailwindcss/nesting'),
         require('tailwindcss'),
     ])
     .options({
-        processCssUrls: false,
+        processCssUrls: true,
     })
     .browserSync({
         server: 'build_local',
